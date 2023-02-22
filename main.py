d3df44c3af7bdf4cc158a44be5ce53297b2504ee
Andrei2738 <andrei.hadarig@gmail.com>
2023-02-22 20:59:19 +0200

    v0.6.1 *removed unnecessary code

--- main.py
+++ main.py
@@ -3,26 +3,6 @@ import random
 
 app = Flask(__name__)
 
-levels = [
-    {
-        "level": 1,
-        "question": "What is 2 + 2?",
-        "answer": "4"
-    },
-    {
-        "level": 2,
-        "question": "What is 3 x 3?",
-        "answer": "9"
-    },
-    {
-        "level": 3,
-        "question": "What is 5 + 5?",
-        "answer": "10"
-    }
-]
-
-current_level = 0
-
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -76,46 +56,3 @@ if __name__ == "__main__":
 
 
 
-class Cube:
-    def __init__(self, value):
-        self.value = value
-
-    def __repr__(self):
-        return f"Cube({self.value})"
-
-class Tower:
-    def __init__(self):
-        self.cubes = []
-
-    def add_cube(self, cube):
-        self.cubes.append(cube)
-
-    def total(self):
-        return sum(cube.value for cube in self.cubes)
-
-    def __repr__(self):
-        return f"Tower({self.cubes})"
-
-class Game:
-    def __init__(self, target_sum):
-        self.target_sum = target_sum
-        self.tower = Tower()
-
-    def generate_cubes(self, count, max_value):
-        return [Cube(random.randint(1, max_value)) for _ in range(count)]
-
-    def play(self):
-        cubes = self.generate_cubes(10, 9)
-        print(f"Cubes: {cubes}, {self.target_sum}")
-        while self.tower.total() < self.target_sum:
-            for index, cube in enumerate(cubes):
-                print(f"{index + 1}. Add cube {cube}")
-            choice = int(input("Enter your choice (1-10): ")) - 1
-            selected_cube = cubes.pop(choice)
-            self.tower.add_cube(selected_cube)
-            print(f"Tower: {self.tower}")
-        print("Congratulations, you have reached the target sum!")
-
-if __name__ == "__main__":
-    game = Game(30)
-    game.play()
