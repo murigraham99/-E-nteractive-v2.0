c7351de3e9d63ce6eef191c465b12b8489f9faa2
Robert Muresan <95537232+murigraham99@users.noreply.github.com>
2023-02-22 20:50:35 +0200

    Blank

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import openai
+
+UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
-
-levels = [
-    {
-        "level": 1,
-        "question": "What is 2 + 2?",
-        "answer": "4"
-    },
-    {
-        "level": 2,
-        "question": "What is 3 x 3?",
-        "answer": "9"
-    },
-    {
-        "level": 3,
-        "question": "What is 5 + 5?",
-        "answer": "10"
-    }
-]
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 current_level = 0
 
-
-@app.route("/upload", methods=["POST"])
-def upload():
-    file = request.files["file"]
-    file.save("C:/Users/ROG/PycharmProjects/E-nteractive/-E-nteractive-v2.0" + file.filename)
-    return jsonify({"message": "File uploaded successfully"})
+OPENAI_API_KEY = "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"
+openai.api_key = OPENAI_API_KEY
 
 
 @app.route("/")
@@ -36,84 +18,45 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/start", methods=["POST"])
+@app.route("/start", methods=["GET", "POST"])
 def start():
-    global current_level
+    global current_level, value, companion
     current_level = 0
-    return render_template("level1.html", level=levels[current_level])
+    if request.method == "POST":
+        value = request.form.get("theme")
+        f = request.files['file']
+        f.save('static/uploads/file.png')
+        print(value)
 
+    if value == "Mickey":
+        companion = "mickey"
+        background = "MickeyHub.png"
 
-# @app.route('/success')
-# def index():
-#     # cu ajutorul render_template putem folosi un template html extern
-#     return render_template("page2.html")
+    elif value == "Rapunzel":
+        companion = "rapunzel"
+        background = "RapunzelHub.png"
 
-
-@app.route("/level1")
-def level1():
-    return render_template("level1.html")
-
-
-@app.route("/level", methods=["POST"])
-def level():
-    global current_level
-    answer = request.form["answer"]
-    if answer == levels[current_level]["answer"]:
-        current_level += 1
-        if current_level >= len(levels):
-            return "You won!"
-        else:
-            return render_template("level1.html", level=levels[current_level])
     else:
-        return render_template("retry.html", level=levels[current_level])
+        companion = "pikachu"
+        background = "PikachuHub.png"
+
+    return render_template("page1.html", background=background, companion=companion)
 
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route("/level3")
+def level3():
 
 
+    class RandomAnimal:
+        def __init__(self):
+            self.LIST_ANIMALS = ["cock.png", "cow.png", "duck.png", "goat.png", "horse.png", "pig.png", "sheep.png"]
 
-class Cube:
-    def __init__(self, value):
-        self.value = value
+        def choose_random(self):
 
-    def __repr__(self):
-        return f"Cube({self.value})"
+    return render_template("level3.html")
 
-class Tower:
-    def __init__(self):
-        self.cubes = []
 
-    def add_cube(self, cube):
-        self.cubes.append(cube)
-
-    def total(self):
-        return sum(cube.value for cube in self.cubes)
-
-    def __repr__(self):
-        return f"Tower({self.cubes})"
-
-class Game:
-    def __init__(self, target_sum):
-        self.target_sum = target_sum
-        self.tower = Tower()
-
-    def generate_cubes(self, count, max_value):
-        return [Cube(random.randint(1, max_value)) for _ in range(count)]
-
-    def play(self):
-        cubes = self.generate_cubes(10, 9)
-        print(f"Cubes: {cubes}, {self.target_sum}")
-        while self.tower.total() < self.target_sum:
-            for index, cube in enumerate(cubes):
-                print(f"{index + 1}. Add cube {cube}")
-            choice = int(input("Enter your choice (1-10): ")) - 1
-            selected_cube = cubes.pop(choice)
-            self.tower.add_cube(selected_cube)
-            print(f"Tower: {self.tower}")
-        print("Congratulations, you have reached the target sum!")
 
 if __name__ == "__main__":
-    game = Game(30)
-    game.play()
-
+    app.run(debug=True, port=4000)
---
+++ static/engine.js
@@ -0,0 +1,143 @@
+let circle = document.querySelector('.circle');
+
+let game1 = document.getElementById('game1');
+const game1_coords = game1.getBoundingClientRect();
+console.log(game1_coords.top, game1_coords.right);
+
+let game2 = document.getElementById('game2');
+const game2_coords = game2.getBoundingClientRect();
+console.log(game2_coords.top, game2_coords.right);
+
+let game3 = document.getElementById('game3');
+const game3_coords = game3.getBoundingClientRect();
+console.log(game3_coords.top, game3_coords.right);
+
+let game4 = document.getElementById('game4');
+const game4_coords = game4.getBoundingClientRect();
+console.log(game4_coords.top, game4_coords.right);
+
+
+let moveBy = 50;
+window.addEventListener('load', () => {
+    circle.style.position = 'absolute';
+    circle.style.left = 0;
+    circle.style.top = 0;
+});
+
+
+
+
+
+
+window.addEventListener('keyup', (e) => {
+    let companion = document.getElementById('companion');
+    const companion_coords = companion.getBoundingClientRect();
+    console.log(companion_coords.top, companion_coords.right);
+
+    console.log("companion coords (Top, Right)=" , companion_coords.top, game1_coords.top)
+
+    console.log("Top dif=" , Math.abs(companion_coords.top - game1_coords.top))
+    console.log( "Right dif=" , Math.abs(companion_coords.right - game1_coords.right))
+
+    if ((Math.abs(companion_coords.top - (game1_coords.top + 160 )) <= 50) && (Math.abs(companion_coords.right - game1_coords.right) <= 50)  ){
+        console.log("Touching 1")
+        document.getElementById("game1").style.backgroundColor = 'red';
+        document.getElementById("game2").style.backgroundColor = '';
+        document.getElementById("game3").style.backgroundColor = '';
+        document.getElementById("game4").style.backgroundColor = '';
+
+          const popUp = document.createElement("div");
+          popUp.className = "pop-up";
+          const message = document.createElement("p");
+          message.textContent = "Play game 1?";
+          const button = document.createElement("button");
+          button.textContent = "Play";
+          button.onclick = () => {
+            window.location.href = "http://127.0.0.1:5000/hub";
+          };
+          popUp.appendChild(message);
+          popUp.appendChild(button);
+          document.body.appendChild(popUp);
+
+    }
+    else if ((Math.abs(companion_coords.top - (game2_coords.top +160 )) <= 50) && (Math.abs(companion_coords.right - game2_coords.right) <= 50)  ){
+        console.log("Touching 2")
+        document.getElementById("game2").style.backgroundColor = 'red';
+        document.getElementById("game1").style.backgroundColor = '';
+        document.getElementById("game3").style.backgroundColor = '';
+        document.getElementById("game4").style.backgroundColor = '';
+
+        const popUp = document.createElement("div");
+          popUp.className = "pop-up";
+          const message = document.createElement("p");
+          message.textContent = "Play game 2?";
+          const button = document.createElement("button");
+          button.textContent = "Play";
+          button.onclick = () => {
+            window.location.href = "http://127.0.0.1:5000/hub";
+          };
+          popUp.appendChild(message);
+          popUp.appendChild(button);
+          document.body.appendChild(popUp);
+
+
+
+    }
+    else if ((Math.abs(companion_coords.top - (game3_coords.top + 160 )) <= 50) && (Math.abs(companion_coords.right - game3_coords.right) <= 50)  ){
+        console.log("Touching 3")
+        document.getElementById("game3").style.backgroundColor = 'red';
+        document.getElementById("game2").style.backgroundColor = '';
+        document.getElementById("game1").style.backgroundColor = '';
+        document.getElementById("game4").style.backgroundColor = '';
+
+        const popUp = document.createElement("div");
+          popUp.className = "pop-up";
+          const message = document.createElement("p");
+          message.textContent = "Play game 3?";
+          const button = document.createElement("button");
+          button.textContent = "Play";
+          button.onclick = () => {
+            window.location.href = "http://127.0.0.1:5000/hub";
+          };
+          popUp.appendChild(message);
+          popUp.appendChild(button);
+          document.body.appendChild(popUp);
+
+    }
+    else if ((Math.abs(companion_coords.top - (game4_coords.top + 160 )) <= 50) && (Math.abs(companion_coords.right - game4_coords.right) <= 50)  ){
+        console.log("Touching 4")
+        document.getElementById("game4").style.backgroundColor = 'red';
+        document.getElementById("game2").style.backgroundColor = '';
+        document.getElementById("game3").style.backgroundColor = '';
+        document.getElementById("game1").style.backgroundColor = '';
+
+        const popUp = document.createElement("div");
+          popUp.className = "pop-up";
+          const message = document.createElement("p");
+          message.textContent = "Play game 4?";
+          const button = document.createElement("button");
+          button.textContent = "Play";
+          button.onclick = () => {
+            window.location.href = "http://127.0.0.1:5000/hub";
+          };
+          popUp.appendChild(message);
+          popUp.appendChild(button);
+          document.body.appendChild(popUp);
+    }
+
+    switch (e.key) {
+        case 'ArrowLeft':
+            circle.style.left = parseInt(circle.style.left) - moveBy + 'px';
+            break;
+        case 'ArrowRight':
+            circle.style.left = parseInt(circle.style.left) + moveBy + 'px';
+            break;
+        case 'ArrowUp':
+            circle.style.top = parseInt(circle.style.top) - moveBy + 'px';
+            break;
+        case 'ArrowDown':
+            circle.style.top = parseInt(circle.style.top) + moveBy + 'px';
+            break;
+
+    }
+});
