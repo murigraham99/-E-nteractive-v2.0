eae9e367c492274ac4330f288f7c93af089967e3
Andrei2738 <andrei.hadarig@gmail.com>
2023-02-23 12:23:32 +0200

    v0.6.4 *finished basic mechanics for level 4

--- static/level4.js
+++ static/level4.js
@@ -1,33 +1,63 @@
-const draggableItems = document.querySelectorAll('.draggable-item');
-const gridItems = document.querySelectorAll('.grid-item');
-let count = 0; // Initialize a count variable to 0
+const draggableItems = document.querySelectorAll('.draggable-item1, .draggable-item2, .draggable-item3, .draggable-item4, .draggable-item5, .draggable-item6, .draggable-item7, .draggable-item8');
+const gridItems = document.querySelectorAll('.grid-item1, .grid-item2, .grid-item3, .grid-item4');
+let count = 0;
 
 draggableItems.forEach(draggableItem => {
-	draggableItem.addEventListener('dragstart', () => {
-		draggableItem.classList.add('dragging');
-	});
+  draggableItem.addEventListener('dragstart', () => {
+    draggableItem.classList.add('dragging');
+  });
 
-	draggableItem.addEventListener('dragend', () => {
-		draggableItem.classList.remove('dragging');
-	});
+  draggableItem.addEventListener('dragend', () => {
+    draggableItem.classList.remove('dragging');
+  });
+
+  draggableItem.addEventListener('dragover', e => {
+    e.preventDefault();
+  });
+
+  draggableItem.addEventListener('drop', e => {
+    e.preventDefault();
+    const draggedItem = document.querySelector('.dragging');
+    const gridItem = e.target.closest('.grid-item');
+    const gridItemTag = gridItem.getAttribute('data-tag');
+    const draggedItemTag = draggedItem.getAttribute('data-tag');
+
+    if (gridItemTag === draggedItemTag) {
+      gridItem.appendChild(draggedItem);
+      count++;
+      console.log(count);
+    } else {
+      alert('The item cannot be dropped here.');
+    }
+  });
 });
 
 gridItems.forEach(gridItem => {
-	gridItem.addEventListener('dragover', e => {
-		e.preventDefault();
-	});
+  gridItem.addEventListener('dragenter', e => {
+    e.preventDefault();
+    gridItem.classList.add('dragging-over');
+  });
 
-	gridItem.addEventListener('drop', e => {
-		const draggedItem = document.querySelector('.dragging');
-		const gridItemTag = gridItem.getAttribute('data-tag'); // Get the tag attribute of the grid item
-		const draggedItemTag = draggedItem.getAttribute('data-tag'); // Get the tag attribute of the dragged item
+  gridItem.addEventListener('dragover', e => {
+    e.preventDefault();
+    gridItem.classList.add('dragging-over');
+  });
 
-		if (gridItemTag === draggedItemTag) {
-			gridItem.appendChild(draggedItem);
-			count++; // Increment the count variable by 1
-			print(count);
-		} else {
-			alert('The item cannot be dropped here.'); // Display an error message
-		}
-	});
+  gridItem.addEventListener('dragleave', () => {
+    gridItem.classList.remove('dragging-over');
+  });
+
+  gridItem.addEventListener('drop', e => {
+    e.preventDefault();
+    const draggedItem = document.querySelector('.dragging');
+    const gridItemTag = gridItem.getAttribute('data-tag');
+    const draggedItemTag = draggedItem.getAttribute('data-tag');
+    if (gridItemTag === draggedItemTag) {
+      gridItem.appendChild(draggedItem);
+      count++;
+      console.log(count);
+    } else {
+      alert('The item cannot be dropped here.');
+    }
+  });
 });
