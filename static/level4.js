08d3deb64e96a7579eddbea100b4411576c37c93
Andrei2738 <andrei.hadarig@gmail.com>
2023-02-24 16:36:49 +0200

    v0.6.5

--- static/level4.js
+++ static/level4.js
@@ -1,6 +1,27 @@
-const draggableItems = document.querySelectorAll('.draggable-item1, .draggable-item2, .draggable-item3, .draggable-item4, .draggable-item5, .draggable-item6, .draggable-item7, .draggable-item8');
+//const draggableItems = document.querySelectorAll('.draggable-item1, .draggable-item2, .draggable-item3, .draggable-item4, .draggable-item5, .draggable-item6, .draggable-item7, .draggable-item8');
 const gridItems = document.querySelectorAll('.grid-item1, .grid-item2, .grid-item3, .grid-item4');
 let count = -1;
+
+const draggableItems = [  { tag: 'glass', img: '/static/images/bin.png' },  { tag: 'glass', img: '/static/images/bin.png' },  { tag: 'plastic', img: '/static/images/plastic1.png' },  { tag: 'plastic', img: '/static/images/plastic2.png' },  { tag: 'paper', img: '/static/images/paper1.png' },  { tag: 'paper', img: '/static/images/paper2.png' },  { tag: 'metal', img: '/static/images/metal1.png' },  { tag: 'metal', img: '/static/images/metal2.png' }];
+
+// Fisher-Yates shuffle algorithm to randomize the order of the draggableItems array
+for (let i = draggableItems.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [draggableItems[i], draggableItems[j]] = [draggableItems[j], draggableItems[i]];
+}
+
+const draggableContainer = document.querySelector('.draggable-container');
+
+for (let i = 0; i < draggableItems.length; i++) {
+  const draggableItem = document.createElement('div');
+  draggableItem.classList.add(`draggable-item${i + 1}`);
+  draggableItem.dataset.tag = draggableItems[i].tag;
+  const img = document.createElement('img');
+  img.src = draggableItems[i].img;
+  draggableItem.appendChild(img);
+  draggableContainer.appendChild(draggableItem);
+}
+
 
 document.body.style.backgroundImage = 'url(/static/images/backgrounds/Level4_bg1.png)';
 
@@ -11,7 +32,7 @@ function setBackground() {
   document.body.style.backgroundImage = imageUrl;
 }
 
-draggableItems.forEach(draggableItem => {
+draggableContainer.childNodes.forEach(draggableItem => {
   draggableItem.addEventListener('dragstart', () => {
     draggableItem.classList.add('dragging');
   });
